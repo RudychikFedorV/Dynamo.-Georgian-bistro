40c104a474d7dc2cbdfce9a760b00e42840526e5
RudychikFedorV <rudychik.fedor@gmail.com>
2024-04-03 20:30:01 +0300

    footer

--- js/index.js
+++ js/index.js
@@ -2,4 +2,14 @@ function navToggle() {
     document.getElementById("headerUl").classList.toggle("_active");
     // document.getElementById("headerBackdrop").classList.toggle("_active");
 }
+
+function openModal(imageUrl) {
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("modalImg").src = imageUrl;
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
 document.getElementById("copyright").textContent = new Date().getFullYear();
